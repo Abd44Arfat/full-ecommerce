048dae0c9ba6892f42f45dd8338331d1da314237
Abdelrahman arfat <abdoarfat924@gmail.com>
2024-04-10 17:40:33 +0200

    finish ci cd

---
+++ lib/features/customer/main/presentation/refactors/main_customer_app_bar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class MainCustomerAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const MainCustomerAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+  
+  @override
+  Size get preferredSize => Size(double.infinity, 70.h);
+}
